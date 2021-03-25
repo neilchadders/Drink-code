1adfce852f352ad301fb9e31f07cdc6889bca5ee
neilchadders <neilchadders@hotmail.com>
2021-03-25 15:32:24 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+*/Task 2
+Customers can order 3 different types of drink and also select 3 sizes.
+Cola, Lemonade and  Orangeade
+Small, Medium and Large
+The button they press have the values cola, lemon and orange
+Create a function named drinkOrder
+Add 2 arguments for size and drink
+Use a switch statement to determine where the functional code needs to be written
+Create a message in each case statement to be returned.
+Console.log the returned message/*
+
+
+function drinkOrder (){
+	
+}
